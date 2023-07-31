b3852b3c71f7ccaf438f35c5c51eb7f50c5abe4b
EphriamHab <ephremhabtamu2015@gmail.com>
2023-07-31 20:48:52 +0300

    AirBnB_clone_v3

---
+++ api/v1/app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''module api/v1/app.py:
+create Flask app; and register the blueprint app_views to Flask instance app
+'''
+
+from os import getenv
+from flask import Flask, jsonify
+
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_engine(exception):
+    '''TearDown:
+    closes the storage on app context teardown
+    removes the current SQLAlchemy Session object after each request
+    '''
+    storage.close()
+
+
+# error handlers for expected app behavior:
+@app.errorhandler(404)
+def not_found(error):
+    '''404:
+    return errmsg `Not Found` '''
+    return jsonify({'error': 'Not found'}), 404
+
+
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
